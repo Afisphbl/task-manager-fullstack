d3458b8ab5038ccc3a01a24d416e69ade5941201
Abduselam Seid <abuabdurehman0308@gmail.com>
2026-04-26 08:06:48 +0300

    feat: restructure task management API with task model, routes, and controllers

---
+++ controller/taskController.js
@@ -0,0 +1,37 @@
+exports.getTasks = (req, res) => {
+  res.status(200).json({
+    status: "success",
+    message: "Get all task",
+  });
+};
+
+exports.getTask = (req, res) => {
+  const { id } = req.params;
+  res.status(200).json({
+    status: "success",
+    message: `Get task with id ${id}`,
+  });
+};
+
+exports.createTask = (req, res) => {
+  res.status(200).json({
+    status: "success",
+    message: `create task with body ${req.body}`,
+  });
+};
+
+exports.updateTask = (req, res) => {
+  const { id } = req.params;
+  res.status(200).json({
+    status: "success",
+    message: `Update task with id ${id}`,
+  });
+};
+
+exports.deleteTask = (req, res) => {
+  const { id } = req.params;
+  res.status(200).json({
+    status: "success",
+    message: `Delete task with id ${id}`,
+  });
+};
--- server.js
+++ server.js
@@ -1,36 +1,18 @@
-const dotenv = require("dotenv")
-dotenv.config({path: "./config.env"});
+const dotenv = require("dotenv");
+const app = require("./app");
 
-const express = require("express");
+const morgan = require("morgan");
 const connectDB = require("./config/db");
-const Task = require("./model/task");
 
-const app = express();
-app.use(express.json());
+dotenv.config({ path: "./config.env" });
 
-// Connect to MongoDB
+const NODE_ENV = process.env.NODE_ENV;
+if (NODE_ENV === "development") app.use(morgan("dev"));
+
 connectDB();
-
-// Create a new task
-const testTask = new Task({
-  title: "Test Task",
-  description: "This is a test task",
-});
-
-testTask
-  .save()
-  .then(() => console.log("Test task created"))
-  .catch((err) => console.error("Error creating test task:", err));
-
-// Routes
-app.use("/api/tasks", (req, res) => {
-  res.send("Hello World");
-});
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(
-    `Server is running on port ${PORT}, connected to MongoDB ${process.env.DATABASE_HOST}`,
-  );
+  console.log(`Server is running on port ${PORT}`);
 });
